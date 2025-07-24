28e64ddee8384d6f834e98b0e6e215197fceab20
d-zucho <danny.nico.dev@outlook.com>
2025-07-23 19:54:27 -0700

    ui: finish feature cards section

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,5 +1,29 @@
+import { div } from 'three/tsl'
+import { abilities } from '../constants'
+
 const FeatureCards = () => {
-  return <div>FeatureCards</div>
+  return (
+    <section className='w-full padding-x-lg'>
+      <div className='mx-auto grid-3-cols'>
+        {abilities.map(({ imgPath, title, desc }) => (
+          <div
+            key={title}
+            className='card-border rounded-xl p-8 flex flex-col gap-4'
+          >
+            <div className='size-14 flex items-center justify-center rounded-full'>
+              <img
+                src={imgPath}
+                alt={title}
+                className='w-full h-full object-contain'
+              />
+            </div>
+            <h3 className='text-white text-2xl font-semibold'>{title}</h3>
+            <div className='text-white-50 text-lg'>{desc}</div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default FeatureCards
